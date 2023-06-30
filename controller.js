0f171b20257b8dfca338c49a0bf297607314911e
Marina Golitsyn <aelmorka@gmail.com>
2023-06-30 18:38:25 +0300

    controller

--- controller.js
+++ controller.js
@@ -4,4 +4,27 @@ $(document).ready(function(){
 
     render.showPosts(tweeter.getPosts())
 
+    $("#post").click(function() {
+        let postInput = $(this).siblings("input")
+        if (postInput.val() != '') {
+            tweeter.addPost(postInput.val())
+            render.showPosts(tweeter.getPosts())
+            postInput.val('')
+        }
+    })
+    $('body').on("click", ".delete", function() {
+        tweeter.removePost($(this).closest(".post").data().id)
+        render.showPosts(tweeter.getPosts())
+    })
+    $('body').on("click", ".delete-comment", function() {
+        tweeter.removeComment($(this).closest(".post").data().id, $(this).closest(".comment").data().id)
+        render.showPosts(tweeter.getPosts())
+    })
+    $('body').on("click", ".comment-btn", function() {
+        let commentIinput = $(this).siblings("input")
+        if (commentIinput.val() != '') {
+            tweeter.addComment(commentIinput.val(), $(this).closest(".post").data().id)
+            render.showPosts(tweeter.getPosts())
+        }
+    })
 })
--- render.js
+++ render.js
@@ -3,19 +3,23 @@ function Render() {
     
     function showPosts(posts) { 
         postList.empty()
-        for (let post in posts) {
-            let postDiv = $(`<div class="post" data-id="${posts[post].id}"><div class="post-text">${posts[post].text}</div></div>`)
-            let delButton = $(`<div class="delete">x</div>`)
-            postDiv.append(delButton)
+        for (let post in posts) { 
+            let postText = '<div class="post" data-id="' + posts[post].id +'"><div class="post-header"><div>' 
+                            + posts[post].text + '</div><div class="delete">x</div></div></div>'
+            let postDiv = $(postText)
             if (posts[post].comments.length != 0) {
                 let commentsDiv = $(`<div class="comments"></div>`)
                 let comments = posts[post].comments
                 for (let comment in comments) {
-                    let com = $(`<div class="comment" data-id="${comments[comment].id}">${comments[comment].text}</div>`)
-                    commentsDiv.append(com)
+                    let comText = '<div class="comment" data-id="' + comments[comment].id + 
+                                '"><span class="delete-comment">x</span>' +
+                                comments[comment].text + '</div>'
+                    commentsDiv.append($(comText))
                 }
                 postDiv.append(commentsDiv)
             }  
+            let newComms = '<div class="comment-twit"><input type="text" /><button class="comment-btn">Comment</button></div>'
+            postDiv.append($(newComms))
             postList.append(postDiv)
         }
     }
